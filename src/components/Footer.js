844ba99dd66fe1c2345a772275ac4892eb54f895
René Nagelhout <rfwnag@hotmail.com>
2022-06-08 10:40:48 +0200

    SongSaver

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            2022 Nailwood Company
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return(
+        <header>
+            <h1>SongSaver deLuxe</h1>
+        </header>
+    )
+}
+
+export default Header
